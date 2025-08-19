e21aff6cdcea268382f1055fa05f6dd5f6d56a8f
Stepan Manaenko <u4zarus@gmail.com>
2025-08-19 17:21:19 +0200

    Setup project

---
+++ src/app/components/PostCard.tsx
@@ -0,0 +1,5 @@
+const PostCard = () => {
+    return <div></div>;
+};
+
+export default PostCard;
---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,12 @@
+interface UserPageProps {
+    params: {
+        id: string;
+    };
+}
+
+const UserPage = async ({ params }: UserPageProps) => {
+    const { id } = await params;
+    return <div>User page {id}</div>;
+};
+
+export default UserPage;
